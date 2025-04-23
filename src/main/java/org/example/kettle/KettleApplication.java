69911527e609e1669a75c8df312ecd49c5ded163
CandyLedge <2648851084@qq.com>
2025-04-23 20:15:05 +0800

    chore: 重构仓库结构

---
+++ src/main/java/org/example/kettle/KettleApplication.java
@@ -0,0 +1,13 @@
+package org.example.kettle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KettleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KettleApplication.class, args);
+    }
+
+}
