737163f1459b991bff15c20ed78cdb00d04190bb
zlf <3215903958@qq.com>
2025-04-26 12:27:37 +0800

    refactor(controller): 移除 StepMapping 类中的未使用代码
    - 删除了整个 makeProcessFile 方法- 清除了导入的未使用类
    使用eza的controller以及数据类

--- src/main/java/kettle/controller/BaiNaiChang/StepMapping.java
+++ src/main/java/kettle/controller/BaiNaiChang/StepMapping.java
@@ -1,22 +1,8 @@
 package kettle.controller.BaiNaiChang;
 
-import cn.hutool.json.JSONUtil;
-import kettle.pojo.Data;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class StepMapping {
-    // 接受步骤json文件
-    @PostMapping("/make_process_file")
-    @ResponseBody
-    public String makeProcessFile(@RequestBody Data json) {
-        System.out.println(json);
-        return JSONUtil.createObj()
-                       .set("status", "200")
-                       .set("msg", "ok")
-                       .toString();
-    }
+
 }
