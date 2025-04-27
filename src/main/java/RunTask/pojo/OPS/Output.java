9ad3782bab9da396ee8214235fc7c70cc5a45316
zlf <3215903958@qq.com>
2025-04-27 13:25:37 +0800

    feat(Output): 封装数据库插入操作
    - 重构 output_database 方法，实现将结果封装为 SQL插入语句
    - 添加 SqlJson 类用于生成 JSON 格式的 SQL 语句
    - 使用 Hutool 的 JSONUtil 进行 JSON 序列化

--- src/main/java/RunTask/pojo/OPS/Output.java
+++ src/main/java/RunTask/pojo/OPS/Output.java
@@ -1,19 +1,62 @@
 package RunTask.pojo.OPS;
 
+import cn.hutool.json.JSONUtil;
+
 import java.util.HashMap;
 import java.util.List;
 
 public class Output {
     // 这里封装了输出的操作 例如：数据库，文件···
     public static void output_database(String cmd, HashMap<String, String> result){
-        System.out.println("cmd = " + cmd);
-        StringBuffer sb = new StringBuffer();
-        sb.append("result -> { \n");
-        for (String key : result.keySet()) {
-            String value = result.get(key);
-            sb.append("\t").append("key").append(" -> ").append(value).append("\n");
-        }
-        sb.append("}\n");
-        System.out.print(sb);
+//        System.out.println("cmd = " + cmd);
+//        StringBuffer sb = new StringBuffer();
+//        sb.append("result -> { \n");
+//        for (String key : result.keySet()) {
+//            String value = result.get(key);
+//            sb.append("\t").append("key").append(" -> ").append(value).append("\n");
+//        }
+//        sb.append("}\n");
+//        System.out.print(sb);
+        String dbName = cmd.split(" ")[1];
+        String tbName = cmd.split(" ")[2];
+        StringBuffer sql = new StringBuffer("insert into ");
+        sql.append(tbName).append("(");
+        String column = String.join(", ",result.keySet());
+        sql.append(column).append(") values (");
+        String values = String.join(", ", result.values());
+        sql.append(values).append(");");
+        SqlJson json = new SqlJson();
+        json.setSql(sql.toString());
+        String jsonStr = JSONUtil.toJsonStr(json);
+        System.out.println("jsonStr = " + jsonStr);
     }
+    private static class SqlJson{
+        private String sql;
+        private List<Object> params;
+        
+        public SqlJson() {
+        }
+        
+        public SqlJson(String sql, List<Object> params) {
+            this.sql = sql;
+            this.params = params;
+        }
+        
+        public String getSql() {
+            return sql;
+        }
+        
+        public void setSql(String sql) {
+            this.sql = sql;
+        }
+        
+        public List<Object> getParams() {
+            return params;
+        }
+        
+        public void setParams(List<Object> params) {
+            this.params = params;
+        }
+    }
+    
 }
