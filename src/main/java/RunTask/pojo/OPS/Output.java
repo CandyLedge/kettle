8a0991cabf2626eab7039e53eb6b305e9d117d28
zlf <3215903958@qq.com>
2025-04-27 14:02:22 +0800

    refactor(Output): 移除未使用的调试代码
    - 删除了 Output 类中不再使用的 StringBuffer 和 System.out.println 语句
    - 保留了数据库输出相关的代码逻辑

--- src/main/java/RunTask/pojo/OPS/Output.java
+++ src/main/java/RunTask/pojo/OPS/Output.java
@@ -8,15 +8,6 @@ import java.util.List;
 public class Output {
     // 这里封装了输出的操作 例如：数据库，文件···
     public static void output_database(String cmd, HashMap<String, String> result){
-//        System.out.println("cmd = " + cmd);
-//        StringBuffer sb = new StringBuffer();
-//        sb.append("result -> { \n");
-//        for (String key : result.keySet()) {
-//            String value = result.get(key);
-//            sb.append("\t").append("key").append(" -> ").append(value).append("\n");
-//        }
-//        sb.append("}\n");
-//        System.out.print(sb);
         String dbName = cmd.split(" ")[1];
         String tbName = cmd.split(" ")[2];
         StringBuffer sql = new StringBuffer("insert into ");
